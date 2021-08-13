307c0a8823a2aed178d873d2f4bdd8058ac7f803
Tuan Anh (Leo) <huynhtuananh21895@gmail.com>
2021-08-13 22:35:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ window.__minimog_menu_data = {
       'https://hta218.myshopify.com/collections'
     ]
   }
-}
+};
 
 (()=>{const e=document.querySelector(".sf-header__desktop");if(e){const t=document.createElement("link");t.rel="stylesheet",t.href="https://cdn.jsdelivr.net/gh/Insights-Labs/minimog-menu-badges/style.css",document.head.appendChild(t),Object.entries(window.__minimog_menu_data||{}).forEach((([t,{bgColor:n,urls:s}])=>{s.forEach((s=>{var o,l;const r=s.replace(window.location.origin,""),a=e.querySelector(`a[href="${r}"]`);if(a&&null!=a&&null!==(o=a.parentNode)&&void 0!==o&&null!==(l=o.classList)&&void 0!==l&&l.contains("sf__menu-item-level3")){const e=document.createElement("span");e.classList.add("mn-badge"),e.style.setProperty("--badge",`"${t}"`),e.style.setProperty("--bg-color",n),a.appendChild(e)}}))}))}})();
