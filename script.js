d80a6f193d5474314ccc734d334770b832f7a7f9
Tuan Anh (Leo) <huynhtuananh21895@gmail.com>
2021-08-13 22:07:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.__minimog_menu_data = {
+  hot: {
+    bgColor: '#f8796e',
+    urls: [
+      'https://hta218.myshopify.com/products/adidas-kids-stan-smith'
+    ]
+  },
+  new: {
+    bgColor: '#9a84c8',
+    urls: [
+      'https://hta218.myshopify.com/products/adidas-classic-backpack',
+      'https://hta218.myshopify.com/collections'
+    ]
+  }
+}
+
